b1cf628b8a7fe0f7b43970247d6c727d677d65a0
Bogdan <107000314+BogdyPG@users.noreply.github.com>
2024-02-15 23:23:02 +0200

    added canvas size changer for ratios based on screen

--- index.js
+++ index.js
@@ -12,11 +12,41 @@ function preload() {
   myshader = loader("testShader");
 }
 
+function getMeasurements(type) {
+  let height;
+  let width;
+  const size = 1.05;
+
+  // truthy
+  if (type == true) {
+    // true screen values, will fit on screen perfectly - scaled to the screen ratio
+    height = window.innerHeight;
+    width = window.innerWidth;
+  }
+  // falsy
+  else if (type == false) {
+    // screen values, will be slightly smaller than the screen - scaled to the screen ratio
+    height = window.innerHeight / size;
+    width = window.innerWidth / size;
+  }
+  // no input
+  else {
+    // equal
+    height = (window.innerHeight < window.innerWidth ? window.innerHeight : window.innerWidth) / size;
+    width = height;
+  }
+  return {height, width};
+}
+
 function setup() {
-  createCanvas(600, 600, WEBGL);
+  clear();
+
+  let {height, width} = getMeasurements();
+  
+  createCanvas(width, height, WEBGL);
   shader(myshader);
 }
 
 function draw() {
-  rect(-width/2, -height/2, width);
+  rect(-width/2, -height/2, width, height);
 }
